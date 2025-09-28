d2d64a430c5c869be63be1f14e9cd6af8ac58533
Bastien Milani <bastienmilani15@gmail.com>
2025-09-28 09:10:17 +0200

    updating doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project     = 'Monalisa Documentation'
+project     = 'Monalisa Renaissance'
 copyright   = '2023, Bastien Milani'
 author      = 'Bastien Milani'
 
